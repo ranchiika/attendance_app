c7c5410cd5464e239fd8097e18aaf3c5b0a918e8
Chika's <fz@Chika-Macbook-Pro.local>
2025-01-20 11:03:36 +0700

    create pop up notification

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,12 +1,111 @@
+import 'package:attendance_app/main.dart';
+import 'package:attendance_app/ui/attend/attendance_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return PopScope( //untuk make sure mau back atau enggak takutnya ada data yang gak kesimpen
+      canPop: false,
+      // ignore: deprecated_member_use
+      onPopInvoked: (didPop) { //
+        if (didPop) {
+          return;
+        }
+        _onWillPop(context);
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Expanded(
+                    child: InkWell(
+                      highlightColor: Colors.transparent,
+                      splashColor: Colors.transparent,
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const AttendanceScreen()));
+                      },
+                      child: const Column(
+                        children: [
+                          const Image(
+                            image: AssetImage('/assets/images/ic_attend.png'),
+                            height: 100,
+                            width: 100,
+                          ),
+                          SizedBox(height: 10,),
+                          Text(
+                            'Attendance',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _onWillPop(BuildContext context) async {
+    return (await showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) =>
+      AlertDialog(
+        title: const Text("INFORMATION",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold
+        ),
+        ),
+        content: const Text(
+          "Do You want to exit",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+          ),
+        ),
+        actions: [
+          TextButton( //No
+            onPressed: ()=> Navigator.of(context).pop(false),
+            child: const Text(
+              "No",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14
+              ),
+            )
+          ),
+
+          TextButton( //Yes
+            onPressed: ()=> SystemNavigator.pop(), //untuk kelua dari aplikasi
+            child: const Text(
+              "Yes",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14
+              ),
+            )
+          ),
+        ],
+      )
+      )
     );
   }
 }
