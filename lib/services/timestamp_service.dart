a4b922881b0bbb665ff1adceb29e0a08653f0c61
Chika's <fz@Chika-Macbook-Pro.local>
2025-01-28 08:19:34 +0700

    update little timestamp_service.dart file

--- lib/services/timestamp_service.dart
+++ lib/services/timestamp_service.dart
@@ -6,7 +6,7 @@ void setDateTime(Function(String, String, String) onDateTimeSet){
   var dateTime = DateFormat('hh:mm:ss');
 
   String date = dateFormat.format(dateNow);
-  String time = dateFormat.format(dateNow);
+  String time = dateTime.format(dateNow);
   String currentDate = '$date | $time';
 
   onDateTimeSet(date, time, currentDate);
