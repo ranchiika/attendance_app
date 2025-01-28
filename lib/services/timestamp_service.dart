567f8c7ab432082d87d8e601c4807e7ede2acdfd
Chika's <fz@Chika-Macbook-Pro.local>
2025-01-28 08:53:29 +0700

    Completing finishing time stamp config

--- lib/services/timestamp_service.dart
+++ lib/services/timestamp_service.dart
@@ -1,6 +1,6 @@
 import 'package:intl/intl.dart';
 
-void setDateTime(Function(String, String, String) onDateTimeSet){
+void setDateTime(Function(String, String, String) onDateTimeSet){ 
   DateTime dateNow = DateTime.now();
   var dateFormat = DateFormat('dd mm yyyy');
   var dateTime = DateFormat('hh:mm:ss');
@@ -10,6 +10,21 @@ void setDateTime(Function(String, String, String) onDateTimeSet){
   String currentDate = '$date | $time';
 
   onDateTimeSet(date, time, currentDate);
+}
 
-  
+void setAttendanceStatus(Function(String) onStatusSet){
+  var dateNow = DateTime.now();
+  var hour = int.parse(DateFormat('hh').format(dateNow));
+  var minute = int.parse(DateFormat('mm').format(dateNow));
+
+  String attendStatus;
+  if (hour < 7 || (hour == 7 && minute == 00)) {
+    attendStatus = "Attend";
+  } else if (hour > 7 || (hour == 7 && minute >= 01)){
+    attendStatus = "Late";
+  } else {
+    attendStatus = "Absent";
+  }
+
+  onStatusSet(attendStatus);
 }
