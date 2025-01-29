0d750386685aa48b3cb6748ad96982d44d90ec83
Chika's <fz@Chika-Macbook-Pro.local>
2025-01-29 10:14:57 +0700

    make a header component

---
+++ lib/ui/attend/components/header.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+Container buildHeader (){
+  return Container(
+    height: 50,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(10),
+        topRight: Radius.circular(10)
+      ),
+      color: Colors.blueAccent
+    ),
+    child: Row(
+      children: [
+        SizedBox(width: 12,),
+        Icon(Icons.face_retouching_natural_outlined,
+        color: Colors.white,
+        ),
+        SizedBox(width: 12,),
+        Text(
+          "Please take a pitcure",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white
+          ),
+        )
+      ],
+    ),
+  );
+}
