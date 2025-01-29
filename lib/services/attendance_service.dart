05185aa121bb6947a0f3ef3fb4e5d23554e74f03
Chika's <fz@Chika-Macbook-Pro.local>
2025-01-29 09:41:59 +0700

    make an app bar

--- lib/services/attendance_service.dart
+++ lib/services/attendance_service.dart
@@ -36,27 +36,27 @@ Future<void> submitAttendanceReport(BuildContext context,String address, String 
       ));
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => HomeScreen())
+        MaterialPageRoute(builder: (context) => const HomeScreen())
       );
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Row(
           children: [
-            Icon(
+            const Icon(
               Icons.info_outline,
               color: Colors.white,
             ),
-            SizedBox(width: 10,),
+            const SizedBox(width: 10,),
             Expanded(
               child: Text(
                 "Ups, $e",
-                style: TextStyle(color: Colors.white),
+                style: const TextStyle(color: Colors.white),
               )
             )
           ],
         ),
         backgroundColor: Colors.blueAccent,
-        shape: StadiumBorder(),
+        shape: const StadiumBorder(),
         behavior: SnackBarBehavior.floating,
       ));
     }
@@ -64,21 +64,21 @@ Future<void> submitAttendanceReport(BuildContext context,String address, String 
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       content: Row(
         children: [
-          Icon(
+          const Icon(
             Icons.error_outline,
             color: Colors.white,
           ),
-          SizedBox(width: 10,),
+          const SizedBox(width: 10,),
           Expanded(
             child: Text(
               "Ups, $error",
-              style: TextStyle(color: Colors.white),
+              style: const TextStyle(color: Colors.white),
             ),
           )
         ],
       ),
       backgroundColor: Colors.blueAccent,
-      shape: StadiumBorder(),
+      shape: const StadiumBorder(),
       behavior: SnackBarBehavior.floating,
     ));
     Navigator.of(context).pop();
@@ -89,12 +89,12 @@ void showLoaderDialog(BuildContext context){
   AlertDialog alert = AlertDialog(
     content: Row(
       children: [
-      CircularProgressIndicator(
+      const CircularProgressIndicator(
         valueColor: AlwaysStoppedAnimation<Color>(Colors.blueAccent),
       ),
       Container(
-        margin: EdgeInsets.only(left: 20),
-        child: Text("Checking the data..."),
+        margin: const EdgeInsets.only(left: 20),
+        child: const Text("Checking the data..."),
         )
     ],),
   );
---
+++ lib/ui/attend/components/app_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+AppBar buildAppBar(BuildContext context){
+  return AppBar(
+    elevation: 0,
+    backgroundColor: Colors.blueAccent,
+    leading: IconButton(
+      icon: Icon(
+        Icons.arrow_back_ios,
+        color: Colors.white,
+      ),
+      onPressed: () => Navigator.of(context).pop(),
+    ),
+    title: Text(
+      "Attendance App",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 18,
+        fontWeight: FontWeight.bold
+      ),
+    ),
+  ); 
+}
